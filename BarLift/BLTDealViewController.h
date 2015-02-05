7a48532126cfd6246a4f73cf32263a120a073adb
Shikhar Mohan <shikharmohan2016@u.northwestern.edu>
2015-02-04 20:47:15 -0600

    added right sliding bar

---
+++ BarLift/BLTDealViewController.h
@@ -0,0 +1,13 @@
+//
+//  BLTDealViewController.h
+//  BarLift
+//
+//  Created by Shikhar Mohan on 2/4/15.
+//  Copyright (c) 2015 Shikhar Mohan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLTDealViewController : UIViewController
+
+@end
---
+++ BarLift/BLTDealViewController.m
@@ -0,0 +1,46 @@
+//
+//  BLTDealViewController.m
+//  BarLift
+//
+//  Created by Shikhar Mohan on 2/4/15.
+//  Copyright (c) 2015 Shikhar Mohan. All rights reserved.
+//
+
+#import "BLTDealViewController.h"
+#import "SWRevealViewController.h"
+
+@interface BLTDealViewController ()
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *sidebarButton;
+
+@end
+
+@implementation BLTDealViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    SWRevealViewController *revealViewController = self.revealViewController;
+    if ( revealViewController )
+    {
+        [self.sidebarButton setTarget: self.revealViewController];
+        [self.sidebarButton setAction: @selector( rightRevealToggle: )];
+        [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
+    }
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
